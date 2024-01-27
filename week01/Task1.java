3348d0d0125452f39fa4fff3d6afd5314ec6bced
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-01-27 13:53:47 +0400

    Added

---
+++ week01/Task1.java
@@ -0,0 +1,9 @@
+//This is our package which must be folder's name
+package week01;
+//This is our class Task1 and it must be file's name 
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hi, Dear user, welcome to our program");
+        System.out.println("I am Ramil Zohrabli");
+    }
+}
