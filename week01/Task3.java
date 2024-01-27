9345f3a5c6b4cf43681849531d795f37231ba48e
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-01-27 16:27:46 +0400

    Changing data types

---
+++ week01/Task3.java
@@ -0,0 +1,21 @@
+package week01;
+
+import java.util.Scanner;
+
+public class Task3 {
+     public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+         //a. Changing data types
+         int a = scan.nextInt();
+         float b = scan.nextFloat();
+         long c = scan.nextLong();
+         //b.Their sum    
+         double sum = a + b + c;
+         //c. The data type of sum should be double because it has wider range and avoid overflow and underflow
+      
+         //d. Printing sum
+         System.out.println(sum);
+
+         scan.close();
+    }
+}
