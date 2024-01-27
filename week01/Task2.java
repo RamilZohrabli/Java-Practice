1dfffd981a8f05eb0f5d4e3fb7342749e0791c14
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-01-27 15:34:27 +0400

    Sum of integer from the user

---
+++ week01/Task2.java
@@ -0,0 +1,19 @@
+package week01;
+import java.util.Scanner;
+public class Task2 {
+    public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+         
+         int a = scan.nextInt();
+         int b = scan.nextInt();
+         int c = scan.nextInt();
+
+         int sum = a+b+c;
+
+         System.out.println(sum);
+
+         scan.close();
+    }
+    
+    
+}
