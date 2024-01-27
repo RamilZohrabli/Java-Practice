a199b065840b8b39a81629006d95558b4c74c1d7
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-01-27 14:52:09 +0400

    Added

--- week01/Task1.java
+++ week01/Task1.java
@@ -2,8 +2,15 @@
 package week01;
 //This is our class Task1 and it must be file's name 
 public class Task1 {
+    //public means that the method is accessible outside this class
+    //static means that the calling of the method can be implemented without instance
+    //void is return type of method 'main'
+    //main is specific method of Java
+    //String[] is an indicator of array's type
+    //args name of parameter
     public static void main(String[] args) {
-        System.out.println("Hi, Dear user, welcome to our program");
+        //println is for print line that it is used printing line of text
+        System.out.println("Hi, Dear user, welcome to Java. Nice to meet you!");
         System.out.println("I am Ramil Zohrabli");
     }
 }
