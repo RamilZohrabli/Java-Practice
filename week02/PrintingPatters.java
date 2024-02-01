bfd30f142f21dadeb81aac8580ac9b3c04f0e1ee
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-02-01 22:21:25 +0400

    Added

---
+++ week02/PrintingPatters.java
@@ -0,0 +1,25 @@
+package week02;
+import java.util.Scanner;
+public class PrintingPatters {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        String[][] Array = new String[number][number];
+        for(int i = 0; i<number ; i++){
+            for(int j = 0; j<number; j++){
+                if(i%2 == 0 && j%2 == 0){
+                    Array[i][j] = "*";
+                }
+                else{
+                    Array[i][j] = "#";
+                }
+            }
+        }
+        for(int i = 0; i<number; i++){
+            for(int j = 0; j<number; j++){
+                System.out.print(Array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
