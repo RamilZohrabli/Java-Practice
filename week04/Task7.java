b2ca76bce3f3b4824dcdbed3c4fcc58dece0e782
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-02-17 14:41:41 +0400

    Added

---
+++ week04/Task7.java
@@ -0,0 +1,18 @@
+package week04;
+import java.util.*;
+public class Task7{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println(reverseString(input));
+    }
+    public static String reverseString(String input) {
+        StringBuilder reversedBuilder = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversedBuilder.append(input.charAt(i));
+        }
+        return reversedBuilder.toString();
+    }
+    
+    
+}
