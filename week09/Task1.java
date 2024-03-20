886ef721588eeb20d510048058e17a377864bd25
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-03-20 14:36:43 +0400

    Added

---
+++ week09/Task1.java
@@ -0,0 +1,16 @@
+package week09;
+import java.util.*;
+class Task {
+    String name;
+    int priority;
+    int duration;
+
+    public Task(String name, int priority, int duration) {
+        this.name = name;
+        this.priority = priority;
+        this.duration = duration;
+    }
+}
+public class Task1 {
+    List<Task> tasks = new ArrayList<>();
+}
