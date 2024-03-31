7551c3dc3fb50b54fe225b2e19e10fd777247dc7
RamilZohrabli <ramilzohrabli310306@gmail.com>
2024-03-31 15:44:33 +0400

    Added

--- week09/Task1.java
+++ week09/Task1.java
@@ -1,32 +1,15 @@
 package week09;
 import java.util.PriorityQueue;
-class Task implements Comparable<Task> {
-    String name;
-    int priority;
-    int duration;
-
-    public Task(String name, int priority, int duration) {
-        this.name = name;
-        this.priority = priority;
-        this.duration = duration;
-    }
-
-    @Override
-    public int compareTo(Task other) {
-        return Integer.compare(other.priority, this.priority);
-    }
-}
-
 public class Task1 {
     public static void main(String[] args) {
-        PriorityQueue<Task> taskQueue = new PriorityQueue<>();
-        taskQueue.add(new Task("Task1", 5, 10));
-        taskQueue.add(new Task("Task2", 15, 20));
+        PriorityQueue<Taskfile> taskQueue = new PriorityQueue<>();
+        taskQueue.add(new Taskfile("Task1", 5, 10));
+        taskQueue.add(new Taskfile("Task2", 15, 20));
         int availableTime = 30;
 
         int totalTimeSpent = 0;
         while (!taskQueue.isEmpty() && availableTime > 0) {
-            Task task = taskQueue.poll();
+            Taskfile task = taskQueue.poll();
             if (task.duration <= availableTime) {
                 System.out.println(task.name);
                 totalTimeSpent += task.duration;
